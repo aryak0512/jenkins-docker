405a867f7b0351139b69a317064907d2d601258e
Aryak Deshpande <aryak.deshpande0512@gmail.com>
2024-05-03 18:45:06 +0530

    Added Jacoco plugin and unit tests

--- src/main/java/com/aryak/test/Calculator.java
+++ src/main/java/com/aryak/test/Calculator.java
@@ -2,22 +2,23 @@ package com.aryak.test;
 
 public class Calculator {
 
-    public int add(int x, int y){
-        return x+y;
+    public int add(int x, int y) {
+        return x + y;
     }
 
-    public int subtract(int x, int y){
-        return x-y;
+    public int subtract(int x, int y) {
+        return x - y;
     }
 
-    public int multiply(int x, int y){
-        return x*y;
+    public int multiply(int x, int y) {
+        return x * y;
     }
-    public int divide(int x, int y){
-        if(y==0){
-            throw new RuntimeException("Division by zero not allowed!");
+
+    public int divide(int x, int y) {
+        if ( y == 0 ) {
+            throw new DivisionByZeroException("Division by zero not allowed!");
         }
-        return x/y;
+        return x / y;
     }
 
 }
--- src/test/java/com/aryak/test/JenkinsDockerApplicationTests.java
+++ src/test/java/com/aryak/test/JenkinsDockerApplicationTests.java
@@ -1,13 +1,28 @@
 package com.aryak.test;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(MockitoExtension.class)
 class JenkinsDockerApplicationTests {
 
 	@Test
-	void contextLoads() {
+	 void applicationContextTest() {
+		JenkinsDockerApplication.main(new String[] {});
+		assertTrue(true);
+	}
+
+	@Test
+	void test_getMessage_is_not_null() {
+
+		JenkinsDockerApplication app = new JenkinsDockerApplication();
+		assertNotNull(app.getMessage());
 	}
 
 }
